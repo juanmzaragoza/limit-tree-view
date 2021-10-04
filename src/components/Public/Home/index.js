c5629c1985a679b0c1296a65214c267996ff6e4e
Juan Manuel Zaragoza <jzaragoza@fi.uba.ar>
2021-10-04 10:15:02 -0300

    Added component to select and filter projects

--- src/components/Public/Home/index.js
+++ src/components/Public/Home/index.js
@@ -8,22 +8,11 @@ import {
 import ProjectsTreeView from "./ProjectsTreeView";
 import SelectOne from "./SelectOne";
 import DetailedContent from "./DetailedContent/index";
-import CostesTable from "./DetailedContent/CostesTable";
-import MaterialTabs from "../../shared/MaterialTabs";
 import ProjectSelector from "./ProjectSelector";
+import PeriodsManagement from "./PeriodsManagement";
 
 const Home = () =>{
   const [show, setShow] = React.useState(false);
-  const [tabs,] = React.useState([
-    { label: "Período 1" },
-    { label: "Período 2" },
-    { label: "Período 3" },
-    { label: "Período 4" },
-    { label: "Período 5" },
-    { label: "Período 6" },
-    { label: "Período 7" },
-    { label: "Período 8" },
-  ])
   return (
     <Container maxWidth={false} sx={{ mt: 4, mb: 4 }}>
       <Grid container spacing={1}>
@@ -32,10 +21,10 @@ const Home = () =>{
         </Grid>
         <Grid item xs={12} md={4} lg={4}>
           <Grid item xs={12} md={12} lg={12}>
-            <MaterialTabs tabs={tabs} />
+            <PeriodsManagement />
           </Grid>
           <Grid container spacing={1}>
-            <Grid item xs={12} md={4} lg={6}>
+            <Grid item xs={12} md={12} lg={12}>
               <Paper
                 sx={{
                   p: 2,
@@ -47,9 +36,6 @@ const Home = () =>{
                 <Typography align="left" variant="h5">Tree View - Proyecto 1</Typography>
                 <ProjectsTreeView onNodeSelect={ids => setShow(!!ids.length)} />
               </Paper>
-            </Grid>
-            <Grid item xs={12} md={4} lg={6}>
-              <CostesTable />
             </Grid>
           </Grid>
         </Grid>
