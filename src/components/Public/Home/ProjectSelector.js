09ef370ce3cb925c909e59d552f4847fee156ebc
amula-limit <amula@limit.es>
2021-10-06 17:23:50 +0200

    Cambio en el ProjectSelector

--- src/components/Public/Home/ProjectSelector.js
+++ src/components/Public/Home/ProjectSelector.js
@@ -1,76 +1,108 @@
 import React from "react";
-import {connect} from "react-redux";
-import {bindActionCreators} from "redux";
-import {Container} from "@mui/material";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Container } from "@mui/material";
 import {
   FormControl,
   Grid,
   InputLabel,
   MenuItem,
   Select,
-  TextField
+  TextField,
 } from "@material-ui/core";
 
 import OutlinedContainer from "components/shared/OutlinedContainer/OutlinedContainer";
 import { getIsLoading, getRows } from "redux/project-selector/selectors";
 import { loadData } from "redux/project-selector";
 
-import "./styles.css"
+import "./styles.css";
 
 const ProjectSelector = ({ onChange, rows, loading, actions }) => {
   const [items, setItems] = React.useState([]);
-  const [project, setProject] = React.useState('');
+  const [project, setProject] = React.useState("");
 
   React.useEffect(() => {
     actions.loadData({});
-  },[]);
+  }, []);
 
   React.useEffect(() => {
-    setItems(rows.map(row => ({ name: row.codi, value: row })));
-  },[rows]);
+    setItems(rows.map((row) => ({ name: row.codi, value: row })));
+  }, [rows]);
 
   const handleChange = (e) => {
     setProject(e.target.value);
     onChange(e.target.value);
-  }
+  };
 
-  return <OutlinedContainer title={"Proyectos"} >
-    <Container maxWidth={false} >
-      <Grid container spacing={4}>
-        <Grid item xs={12} md={3} lg={3}>
-          <FormControl fullWidth >
-            <InputLabel id="project-number-label">Número de proyecto</InputLabel>
-            <Select
-              labelId="project-number-label"
-              id="project-number"
-              name="project-number"
-              value={project}
-              label="Número de proyecto"
-              onChange={handleChange}
-            >
-              {items.map((item,index) => <MenuItem key={index} value={item.value}>{item.name}</MenuItem>)}
-            </Select>
-          </FormControl>
+  return (
+    <OutlinedContainer title={"Proyectos"}>
+      <Container maxWidth={false}>
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={3} lg={2}>
+            <FormControl fullWidth>
+              <InputLabel id="project-number-label">
+                Número de proyecto
+              </InputLabel>
+              <Select
+                labelId="project-number-label"
+                id="project-number"
+                name="project-number"
+                value={project}
+                label="Número de proyecto"
+                onChange={handleChange}
+              >
+                {items.map((item, index) => (
+                  <MenuItem key={index} value={item.value}>
+                    {item.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={3}>
+            <FormControl fullWidth>
+              <TextField
+                disabled
+                id="outlined-basic"
+                label="Proyecto"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                value={project.nom}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={4}>
+            <FormControl fullWidth>
+              <TextField
+                id="outlined-basic"
+                label="Cliente"
+                disabled
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                value={project.client ? project.client?.description  : "" }
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={3}>
+            <FormControl fullWidth>
+              <TextField
+                id="outlined-basic"
+                label="Estado"
+                disabled
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                value={project.estat ? project.estat : ""}
+              />
+            </FormControl>
+          </Grid>
         </Grid>
-        <Grid item xs={2}>
-          <FormControl fullWidth>
-            <TextField id="outlined-basic" label="Proyecto" disabled value={project.nom} />
-          </FormControl>
-        </Grid>
-        <Grid item xs={4}>
-          <FormControl fullWidth>
-            <TextField id="outlined-basic" label="Cliente" disabled value={project.client?.description}/>
-          </FormControl>
-        </Grid>
-        <Grid item xs={3}>
-          <FormControl fullWidth>
-            <TextField id="outlined-basic" label="Estado" disabled value={project.estat} />
-          </FormControl>
-        </Grid>
-      </Grid>
-    </Container>
-  </OutlinedContainer>;
-}
+      </Container>
+    </OutlinedContainer>
+  );
+};
 
 const mapStateToProps = (state, props) => {
   return {
@@ -81,10 +113,10 @@ const mapStateToProps = (state, props) => {
 
 const mapDispatchToProps = (dispatch, props) => {
   const actions = {
-    loadData: bindActionCreators(loadData, dispatch)
+    loadData: bindActionCreators(loadData, dispatch),
   };
   return { actions };
 };
 
-const component = connect(mapStateToProps,mapDispatchToProps)(ProjectSelector);
+const component = connect(mapStateToProps, mapDispatchToProps)(ProjectSelector);
 export default component;
