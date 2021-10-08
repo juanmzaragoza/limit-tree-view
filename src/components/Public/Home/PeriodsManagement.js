73367416323bcf2b611971014b666aadd522d7ee
amula-limit <amula@limit.es>
2021-10-08 10:40:25 +0200

    Cambiar estado periodo si esta cerrado

--- src/components/Public/Home/PeriodsManagement.js
+++ src/components/Public/Home/PeriodsManagement.js
@@ -51,7 +51,7 @@ const PeriodsManagement = ({
       }))
     );
   }, [rows]);
-  console.log(tancat);
+
   return (
     <Grid container spacing={1} direction="row" alignItems="center">
       <Grid item xs={12} md={12} lg={8}>
