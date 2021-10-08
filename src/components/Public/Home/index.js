9f616a89a88daf7c60acddaed00b39f5515ee537
Juan Manuel Zaragoza <jzaragoza@fi.uba.ar>
2021-10-08 12:22:57 -0300

    Autocomplete component

--- src/components/Public/Home/index.js
+++ src/components/Public/Home/index.js
@@ -32,7 +32,7 @@ const Home = ({ data, loading, selectedPeriod, actions }) =>{
   return ( <Container maxWidth={false} sx={{ mt: 4, mb: 4 }}>
       <Grid container>
         <Grid item xs={12} md={12} lg={12} >
-          <ProjectSelector onChange={(e) => console.log(e)} />
+          <ProjectSelector />
         </Grid>
 
         <Grid item xs={12} md={4} lg={4} sx={{ mt: 2, mb: 3 }}>
---
+++ src/components/shared/MaterialAutocomplete/MaterialAsyncAutocomplete.js
@@ -0,0 +1,108 @@
+import * as React from "react";
+import { useState } from "react";
+import { isEqual } from "lodash";
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import CircularProgress from '@mui/material/CircularProgress';
+
+const MaterialAsyncAutocomplete = ({
+  id = "asynchronous",
+  label,
+  variant = "standard",
+  loading = false,
+  items = [],
+  onChange = () => {},
+  onSearch = () => {}
+}) => {
+  const [value, setValue] = React.useState('');
+  const [open, setOpen] = React.useState(false);
+  const [options, setOptions] = React.useState([]);
+  const [active, setActive] = useState(true);
+  const loadingComponent = (open && options.length === 0) || loading;
+
+  React.useEffect(() => {
+    setActive(true);
+
+    if (!loadingComponent) {
+      return undefined;
+    }
+
+    return () => {
+      setActive(false);
+    };
+  }, [loadingComponent]);
+
+  React.useEffect(() => {
+    if (!open) {
+      setOptions([]);
+    } else{
+      setOptions([...items]);
+    }
+  }, [open]);
+
+  React.useEffect(() => {
+    if (active) {
+      setOptions([...items]);
+    }
+  },[items]);
+
+  const handleChange = (e,v,d) => {
+    setValue(v);
+    onChange(e,v,d);
+  };
+
+  let timer;
+  const handleInputChange = (e,v,r) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      if(r !== "reset"){
+        onSearch(e,v,r);
+      }
+    },1000);
+  };
+
+  return (
+    <React.Fragment>
+      <Autocomplete
+        id={id}
+        value={value}
+        open={open}
+        onOpen={() => {
+          setOpen(true);
+        }}
+        onClose={() => {
+          setOpen(false);
+        }}
+        isOptionEqualToValue={(option, value) => isEqual(option, value)}
+        getOptionLabel={(option) => {
+          if(!option) return "";
+          return option.label;
+        }}
+        onChange={handleChange}
+        onInputChange={handleInputChange}
+        options={options}
+        loading={loadingComponent}
+        loadingText={"Cargando..."}
+        noOptionsText={"No hay opciones disponibles"}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label={label}
+            variant={variant}
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <React.Fragment>
+                  {loadingComponent ? <CircularProgress color="inherit" size={20} /> : null}
+                  {params.InputProps.endAdornment}
+                </React.Fragment>
+              ),
+            }}
+          />
+        )}
+      />
+    </React.Fragment>
+  );
+}
+
+export default MaterialAsyncAutocomplete;
