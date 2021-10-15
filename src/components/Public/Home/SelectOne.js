9471ceda35b3c966f277de20097b48c7eaa3e3cd
amula-limit <amula@limit.es>
2021-10-15 16:21:13 +0200

    Agregar Logos

--- src/components/Public/Home/SelectOne.js
+++ src/components/Public/Home/SelectOne.js
@@ -1,7 +1,16 @@
-import {Typography} from "@mui/material";
+import { Grid } from "@mui/material";
+import logoLimit from "../../../utils/logo_limit.svg";
 
 const SelectOne = () => {
-  return <Typography variant={"h3"} >{"<= Seleccione 1"}</Typography>
-}
+  return (
+    <Grid container>
+      <Grid item lg={4}></Grid>
+      <Grid item lg={4}>
+        <img src={logoLimit} className="logoLimit" />
+      </Grid>
+    </Grid>
+  );
+};
+
 
 export default SelectOne;
