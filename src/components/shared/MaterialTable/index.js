1666794ecdd21a202d2a393420203e0ee7c3ef63
amula-limit <amula@limit.es>
2021-10-19 08:14:21 +0200

    Agregar Tooltip al boton costesReales

--- src/components/shared/MaterialTable/index.js
+++ src/components/shared/MaterialTable/index.js
@@ -14,6 +14,7 @@ import {
   IconButton,
   Stack,
   CircularProgress,
+  Tooltip,
 } from "@mui/material";
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
 import DialogCostes from "./DialogCostes";
@@ -61,7 +62,7 @@ const MaterialTable = ({
   actions,
   costs,
   loading,
-  loadingTable
+  loadingTable,
 }) => {
   const classes = useStyles();
 
@@ -72,126 +73,66 @@ const MaterialTable = ({
     setOpen(true);
   };
 
-  return (
-    loadingTable ? (
-      <Stack sx={{ color: "grey.500", alignItems: "center", marginTop:"130px" }}>
-        <CircularProgress color="inherit" />
-      </Stack>
-    ) : (
+  return loadingTable ? (
+    <Stack sx={{ color: "grey.500", alignItems: "center", marginTop: "130px" }}>
+      <CircularProgress color="inherit" />
+    </Stack>
+  ) : (
     <Paper sx={{ width: "100%" }}>
-  
-        <TableContainer className={classes.stickyActionsColumn}>
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow className="tableRowBorder">
-                {groups.map((column, index) => {
-                  return (
-                    <TableCell
-                      align="center"
-                      colSpan={column.colSpan}
-                      sx={{ fontWeight: "bold" }}
-                      key={index}
-                      className={column.className}
-                    >
-                      {column.label}
-                    </TableCell>
-                  );
-                })}
-              </TableRow>
-              <TableRow sx={{}}>
-                {columns.map((column, index) => (
+      <TableContainer className={classes.stickyActionsColumn}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow className="tableRowBorder">
+              {groups.map((column, index) => {
+                return (
                   <TableCell
+                    align="center"
+                    colSpan={column.colSpan}
+                    sx={{ fontWeight: "bold" }}
                     key={index}
-                    style={{
-                      top: 57,
-                      minWidth: column.minWidth,
-                      fontWeight: "bold",
-                    }}
-                    align={column.numeric ? "right" : "left"}
                     className={column.className}
                   >
                     {column.label}
                   </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {content.map((row, index) => {
-                return (
-                  <TableRow
-                    hover
-                    tabIndex={-1}
-                    key={index}
-                    onDoubleClick={(e) => onDoubleClick(row)}
-                  >
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      const value2 = row[column.id2];
-
-                      return (
-                        <TableCell
-                          key={column.id}
-                          align={column.numeric ? "right" : "left"}
-                          className={column.className}
-                        >
-                          {value === undefined && "---"}
-                          <span
-                            className={
-                              column.colorValue
-                                ? value > 0
-                                  ? classes.colorGreen
-                                  : value < 0
-                                  ? classes.colorRed
-                                  : ""
-                                : ""
-                            }
-                          >
-                            {column.format && typeof value === "number"
-                              ? column.format(value)
-                              : value}{" "}
-                          </span>
-                          {value2
-                            ? column.format && typeof value === "number"
-                              ? column.format(value2)
-                              : ` - ${value2}`
-                            : ""}
-                          <span style={{ float: "right" }}>
-                            {column.button && (
-                              <React.Fragment>
-                                <IconButton
-                                  aria-label="upload picture"
-                                  component="span"
-                                  onClick={() => handleClick(row.id)}
-                                  color={"info"}
-                                >
-                                  <MonetizationOnIcon />
-                                </IconButton>
-                              </React.Fragment>
-                            )}
-                          </span>
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
                 );
               })}
-              <DialogCostes
-                open={open}
-                onClose={() => setOpen(false)}
-                contentDialog={costs}
-                loading={loading}
-              />
-              <TableRow>
-                <TableCell sx={{ fontWeight: "bold" }}>TOTAL</TableCell>
-                {columnsSubTotal.map((column) => {
-                  const value = contentTotal[column.id];
-                  return (
-                    <TableCell
-                      key={column.id}
-                      align={column.numeric ? "right" : "left"}
-                      className={column.className}
-                    >
-                      <strong>
+            </TableRow>
+            <TableRow sx={{}}>
+              {columns.map((column, index) => (
+                <TableCell
+                  key={index}
+                  style={{
+                    top: 57,
+                    minWidth: column.minWidth,
+                    fontWeight: "bold",
+                  }}
+                  align={column.numeric ? "right" : "left"}
+                  className={column.className}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {content.map((row, index) => {
+              return (
+                <TableRow
+                  hover
+                  tabIndex={-1}
+                  key={index}
+                  onDoubleClick={(e) => onDoubleClick(row)}
+                >
+                  {columns.map((column) => {
+                    const value = row[column.id];
+                    const value2 = row[column.id2];
+
+                    return (
+                      <TableCell
+                        key={column.id}
+                        align={column.numeric ? "right" : "left"}
+                        className={column.className}
+                      >
                         {value === undefined && "---"}
                         <span
                           className={
@@ -206,19 +147,77 @@ const MaterialTable = ({
                         >
                           {column.format && typeof value === "number"
                             ? column.format(value)
-                            : value}
+                            : value}{" "}
                         </span>
-                      </strong>
-                    </TableCell>
-                  );
-                })}
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
-      
+                        {value2
+                          ? column.format && typeof value === "number"
+                            ? column.format(value2)
+                            : ` - ${value2}`
+                          : ""}
+                        <span style={{ float: "right" }}>
+                          {column.button && (
+                            <React.Fragment>
+                              <Tooltip title="Costes Reales" arrow>
+                                <IconButton
+                                  aria-label="upload picture"
+                                  component="span"
+                                  onClick={() => handleClick(row.id)}
+                                  color={"info"}
+                                >
+                                  <MonetizationOnIcon />
+                                </IconButton>
+                              </Tooltip>
+                            </React.Fragment>
+                          )}
+                        </span>
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+            <DialogCostes
+              open={open}
+              onClose={() => setOpen(false)}
+              contentDialog={costs}
+              loading={loading}
+            />
+            <TableRow>
+              <TableCell sx={{ fontWeight: "bold" }}>TOTAL</TableCell>
+              {columnsSubTotal.map((column) => {
+                const value = contentTotal[column.id];
+                return (
+                  <TableCell
+                    key={column.id}
+                    align={column.numeric ? "right" : "left"}
+                    className={column.className}
+                  >
+                    <strong>
+                      {value === undefined && "---"}
+                      <span
+                        className={
+                          column.colorValue
+                            ? value > 0
+                              ? classes.colorGreen
+                              : value < 0
+                              ? classes.colorRed
+                              : ""
+                            : ""
+                        }
+                      >
+                        {column.format && typeof value === "number"
+                          ? column.format(value)
+                          : value}
+                      </span>
+                    </strong>
+                  </TableCell>
+                );
+              })}
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Paper>
-    )
   );
 };
 
@@ -226,7 +225,6 @@ const mapStateToProps = (state, props) => {
   return {
     costs: getCost(state),
     loading: getIsLoading(state),
-   
   };
 };
 
