3f849c2932597646d685d8f1993dded0a145f579
Juan Manuel Zaragoza <jzaragoza@fi.uba.ar>
2021-10-20 09:02:55 -0300

    Little redux refactor

--- src/redux/example/selectors.js
+++
@@ -1,5 +0,0 @@
-import { get } from "lodash";
-
-export const getData = state => get(state, "email.data", false);
-export const getIsLoading = state => get(state, "email.loading");
-export const getIsLoaded = state => get(state, "email.loaded");
