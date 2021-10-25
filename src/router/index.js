1365951930ba32c9d1b07db8e869cfe4e0cf2665
Juan Manuel Zaragoza <jzaragoza@fi.uba.ar>
2021-10-25 12:17:14 -0300

    Typo

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Router, Redirect, Switch, Route } from "react-router";
 import { createBrowserHistory } from "history";
 
-import Public from "components/Layout/Private/PrivateRoute";
+import Private from "components/Layout/Private/PrivateRoute";
 import Home from "components/Private/Home";
 import Login from "components/Login";
 
 const Routes = () => (
   <Router history={createBrowserHistory()}>
     <Switch>
-      <Public path={PATHNAME.INDEX} component={Home} exact />
+      <Private path={PATHNAME.INDEX} component={Home} exact />
       <Route path={PATHNAME.LOGIN} component={Login} exact />
       <Redirect from="*" to={PATHNAME.INDEX} />
     </Switch>
